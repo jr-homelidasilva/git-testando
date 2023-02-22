2833ddc1e69d3d67c671e92e52c490675b44ca67
josinho <robertohomeli@yahoo.com.br>
2023-02-22 03:29:03 -0300

    adding grosseria

---
+++ grosseria.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 23:19:22 2023
+
+@author: rober
+"""
+
+teste = 1
+
+def soma1(n):
+    print("Aqui eu somo 1 a inteiros :D")
+    if type(n) == int:
+        n +=1
+        return n
+    else:
+        print("epa, epa! bota um inteiro na próxima!")
+        
